98bce7bbbc3109a736b49fff2792285778cede19
alia-dd <91740777+alia-dd@users.noreply.github.com>
2023-05-23 14:33:05 +0300

    Delete rcfisrt directory

--- rcfisrt/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Hook from './hook';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Hook/>);
-
-
